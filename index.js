610bfc3909a86d621e34d2157dcf5640152dd74d
Jen Dusseljee <jendusseljee@gmail.com>
2020-05-25 00:19:08 +0200

    made server attempt to reconnect every 5 seconds

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
 'use strict';
 
-const { Bulb } = require('./yeelight.js');
+const Bulb = require('./yeelight.js');
 const http = require('http');
 
-const yeelights = [new Bulb('192.168.0.150'), new Bulb('192.168.0.151'), new Bulb('192.168.0.152')];
+const bulbs = [new Bulb('192.168.0.150'), new Bulb('192.168.0.151'), new Bulb('192.168.0.152')];
+
+const reconnect = function () {
+   let bulb;
+   for (bulb of bulbs) {
+      if (!bulb.connected)
+         bulb.connect();
+   }
+};
 
 const moody = function () {
-  var bulb;
-  for (bulb of yeelights){
-     bulb.onn();
-     bulb.color(255, 255, 255);
-     bulb.temperature(2000);
-     bulb.brightness(1);
-  }
+   let bulb;
+   for (bulb of bulbs) {
+      bulb.onn();
+      bulb.temperature(2000);
+      bulb.brightness(20);
+   }
 };
 
 const toggle = function () {
-   var bulb;
-   for (bulb of yeelights){
+   let bulb;
+   for (bulb of bulbs) {
       bulb.toggle();
    }
 };
 
 
-const requestListener = function(req, res) {
+const requestListener = function (req, res) {
    switch (req.url) {
       case '/moody':
          moody();
          break;
       case '/toggle':
          toggle();
-         break
+         break;
+      case '/reconnect':
+         reconnect();
+         break;
    }
 
    res.writeHead(200);
@@ -39,11 +49,13 @@ const requestListener = function(req, res) {
 
 const server = http.createServer(requestListener);
 
-var bulb;
-for (bulb of yeelights) {
-   bulb.on('error', (error) => console.log(error));
-   bulb.on('disconnected', () => bulb.connect());
+let bulb;
+for (bulb of bulbs) {
+   bulb.on('error', (light, error) => console.log(error));
+   bulb.on('disconnected', () => console.log('disconnected'));
    bulb.connect();
 }
+
+setInterval(reconnect, 5000);
 
 server.listen(3000);
