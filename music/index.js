018fa32bacba9fd879dc004349e7a2964515a149
Jen Dusseljee <jendusseljee@gmail.com>
2020-06-05 01:29:21 +0200

    Added endpoint to get image.

--- music/index.js
+++ music/index.js
@@ -1,6 +1,8 @@
 const { Sonos } = require('sonos');
+const fs = require('fs');
+const http = require('http');
 
-const device = new Sonos('192.168.0.155');
+const device = new Sonos('192.168.0.155', 1400);
 
 async function routes(fastify, options, done) {
     fastify.get('/play', async (request, reply) => {
@@ -19,6 +21,15 @@ async function routes(fastify, options, done) {
     });
     fastify.get('/volume', async (request, reply) => {
         return device.getVolume();
+    });
+    fastify.get('/current/image.png', async (request, reply) => {
+        let file = fs.createWriteStream("image.png");
+        let currentTrack = await device.currentTrack();
+        http.get(currentTrack['albumArtURL'], function(response) {
+            response.pipe(file).on('close', () => fs.readFile('image.png', (err, fileBuffer) => {
+                reply.send(err || fileBuffer);
+            }));
+        });
     });
     done()
 }
